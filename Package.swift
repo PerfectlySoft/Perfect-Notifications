bab4f8e786cd3bd45ef772e18c5790bd7351d2ce
Kyle Jessup <badthing@gmail.com>
2017-09-20 14:58:17 -0400

    Bumped to 3.0

--- Package.swift
+++ Package.swift
@@ -1,32 +1,14 @@
-//
-//  Package.swift
-//  PerfectNotifications
-//
-//  Created by Kyle Jessup on 4/20/16.
-//	Copyright (C) 2016 PerfectlySoft, Inc.
-//
-//===----------------------------------------------------------------------===//
-//
-// This source file is part of the Perfect.org open source project
-//
-// Copyright (c) 2015 - 2016 PerfectlySoft Inc. and the Perfect project authors
-// Licensed under Apache License v2.0
-//
-// See http://perfect.org/licensing.html for license information
-//
-//===----------------------------------------------------------------------===//
-//
-
+// Generated automatically by Perfect Assistant Application
+// Date: 2017-09-20 18:53:23 +0000
 import PackageDescription
-
 let package = Package(
-    name: "PerfectNotifications",
-    targets: [],
-    dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 2),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 1)
+	name: "PerfectNotifications",
+	targets: [],
+	dependencies: [
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
+		.Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 3),
 	]
 )
