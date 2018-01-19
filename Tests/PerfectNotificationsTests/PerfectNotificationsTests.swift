f4863c4ea319ea2dd1bbb49dae29889e97e2719a
RockfordWei <rocky@perfect.org>
2018-01-19 10:28:25 -0500

    Fixing swift testing error on linux.

--- Tests/PerfectNotificationsTests/PerfectNotificationsTests.swift
+++ Tests/PerfectNotificationsTests/PerfectNotificationsTests.swift
@@ -3,5 +3,7 @@ import PerfectLib
 @testable import PerfectNotifications
 
 class PerfectNotificationsTests: XCTestCase {
-
+ static var allTests : [(String, (PerfectNotificationsTests) -> () throws -> ())] {
+        return []
+ }
 }
