feb6bf42ef7301448cec0e066d824084df851d9d
Kyle Jessup <kyle@perfect.org>
2018-04-11 15:29:07 -0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -1,14 +1,19 @@
-// Generated automatically by Perfect Assistant Application
-// Date: 2017-09-20 18:53:23 +0000
+// swift-tools-version:4.0
 import PackageDescription
+
 let package = Package(
 	name: "PerfectNotifications",
-	targets: [],
+	products: [
+		.library(name: "PerfectNotifications", targets: ["PerfectNotifications"])
+	],
 	dependencies: [
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", majorVersion: 3), /* for HTTP2Client */
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", majorVersion: 3),
-		.Package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", majorVersion: 3),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", from: "3.1.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTPServer.git", from: "3.0.12"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-HTTP.git", from: "3.0.12"),
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "3.0.0")
+	],
+	targets: [
+		.target(name: "PerfectNotifications", dependencies: ["PerfectHTTPServer", "PerfectNet", "PerfectHTTP", "PerfectThread", "PerfectCrypto"])
 	]
 )
