8c5508cdff46c33c1ff819035c75fd3a5d680cb0
Kyle Jessup <kyle.jessup@treefrog.ca>
2018-04-11 15:30:24 -0400

    git out

--- Tests/PerfectNotificationsTests/PerfectNotificationsTests.swift
+++
@@ -1,9 +0,0 @@
-import XCTest
-import PerfectLib
-@testable import PerfectNotifications
-
-class PerfectNotificationsTests: XCTestCase {
- static var allTests : [(String, (PerfectNotificationsTests) -> () throws -> ())] {
-        return []
- }
-}
