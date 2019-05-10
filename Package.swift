80d079a24c9415a0fa082fa85e3066dc785492da
Kyle Jessup <kyle.jessup@treefrog.ca>
2019-05-09 21:18:23 -0400

    Removed direct crypto dep

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.1
 import PackageDescription
 
 let package = Package(
@@ -8,10 +8,9 @@ let package = Package(
 	],
 	dependencies: [
 		.package(url: "https://github.com/PerfectlySoft/Perfect-Net.git", from: "3.1.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0"),
-		.package(url: "https://github.com/PerfectlySoft/Perfect-Crypto.git", from: "3.0.0")
+		.package(url: "https://github.com/PerfectlySoft/Perfect-Thread.git", from: "3.0.0")
 	],
 	targets: [
-		.target(name: "PerfectNotifications", dependencies: ["PerfectNet", "PerfectThread", "PerfectCrypto"])
+		.target(name: "PerfectNotifications", dependencies: ["PerfectNet", "PerfectThread"])
 	]
 )
